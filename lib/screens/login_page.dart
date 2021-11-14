96404fa89b6a339444f179f807e5ff685c99d1d2
dagsis <dagsis@dagsis.com.ar>
2021-11-14 08:48:54 -0300

    Login 2

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -8,10 +8,118 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _email = '';
+  String _password = '';
+  bool _rememberme = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login Page')),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _showLog(),
+          SizedBox(
+            height: 20,
+          ),
+          _showEmail(),
+          _showPassword(),
+          _showRememberme(),
+          _showButton(),
+        ],
+      )),
+    );
+  }
+
+  Widget _showLog() {
+    return Image(
+      image: AssetImage('assets/vehicles_logo.png'),
+      width: 300,
+    );
+  }
+
+  Widget _showEmail() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            hintText: 'Ingrese su email...',
+            labelText: 'Email',
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            suffixIcon: Icon(Icons.email)),
+        onChanged: (value) {
+          _email = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showPassword() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            hintText: 'Ingrese su password...',
+            labelText: 'Password',
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            suffixIcon: Icon(Icons.lock)),
+        onChanged: (value) {
+          _password = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showRememberme() {
+    return CheckboxListTile(
+        title: Text('Recordarme'),
+        value: _rememberme,
+        onChanged: (value) {
+          setState(() {
+            _rememberme = value!;
+          });
+        });
+  }
+
+  Widget _showButton() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('Iniciar Sesión'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                  return Color(0xFF120e43);
+                }),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('Nuevo Usuario'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                  return Color(0xFFE033888);
+                }),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
